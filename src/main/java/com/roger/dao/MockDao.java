3e929456384bf1877b869aabf6374b29f6c4bcff
yudar1024 <rojer.chen@foxmail.com>
2015-05-06 15:02:04 +0800

    update project name

--- src/main/java/com/roger/dao/MockDao.java
+++
@@ -1,5 +0,0 @@
-package com.roger.dao;
-
-public interface MockDao {
-
-}
--- src/main/java/com/roger/dao/impl/MockDaoImpl.java
+++
@@ -1,7 +0,0 @@
-package com.roger.dao.impl;
-
-import com.roger.dao.MockDao;
-
-public class MockDaoImpl implements MockDao {
-
-}
